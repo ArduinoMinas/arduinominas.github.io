cf2b170ebb685775feebd17985cfbdb7f5ca6c6e
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-03-28 20:29:19 -0300

    Reenviando o plugin, a indentação falhou ao ser copiado.

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -17,34 +17,33 @@
 #
 
 module Jekyll
-    
-    class ImageTag < Liquid::Tag
-        @img = nil
-        
-        def initialize(tag_name, markup, tokens)
-            attributes = ['class', 'src', 'width', 'height', 'title']
-            
-            if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-                @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-                if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
-                    @img['title']  = title
-                    @img['alt']    = alt
-                    else
-                    @img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
-                                                         end
-                                                         @img['class'].gsub!(/"/, '') if @img['class']
-                                                                             end
-                                                                             super
-                                                                             end
+	class ImageTag < Liquid::Tag
+		@img = nil
+
+		def initialize(tag_name, markup, tokens)
+			attributes = ['class', 'src', 'width', 'height', 'title']
+
+			if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+				@img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+				if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
+					@img['title']  = title
+					@img['alt']    = alt
+				else
+					@img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
+				end
+				@img['class'].gsub!(/"/, '') if @img['class']
+			end
+			super
+		end
+
+		def render(context)
+			if @img
+				"<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
+			else
+				"Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
+			end
+		end
+	end
+end
                                                                              
-                                                                             def render(context)
-                                                                             if @img
-                                                                             "<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
-                                                                             else
-                                                                             "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
-                                                                             end
-                                                                             end
-                                                                             end
-                                                                             end
-                                                                             
-                                                                             Liquid::Template.register_tag('img', Jekyll::ImageTag)
+Liquid::Template.register_tag('img', Jekyll::ImageTag)

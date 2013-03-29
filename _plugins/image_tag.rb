72f6648db9723968e3a928e863e9ff410760611d
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-03-28 21:49:54 -0300

    Teste de um novo plugin para image e correção da tag.

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -1,49 +1,63 @@
-# Title: Simple Image tag for Jekyll
-# Authors: Brandon Mathis http://brandonmathis.com
-#          Felix Schäfer, Frederic Hemberger
-# Description: Easily output images with optional class names, width, height, title and alt attributes
-#
-# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
-#
-# Examples:
-# {% img /images/ninja.png Ninja Attack! %}
-# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
-# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
-#
-# Output:
-# <img src="/images/ninja.png">
-# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
-# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
-#
-
 module Jekyll
-	class ImageTag < Liquid::Tag
-		@img = nil
+  class ImageBlock < Liquid::Block
+    def initialize(tag_name, filename, tokens)
+      super
+      @image_name = filename.strip
+    end
 
-		def initialize(tag_name, markup, tokens)
-			attributes = ['class', 'src', 'width', 'height', 'title']
+    def render(context)
+      load_options block_contents
 
-			if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-				@img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-				if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @img['title']
-					@img['title']  = title
-					@img['alt']    = alt
-				else
-					@img['alt']    = @img['title'].gsub!(/"/, '&#34;') if @img['title']
-				end
-				@img['class'].gsub!(/"/, '') if @img['class']
-			end
-			super
-		end
+      if link?
+        "<a href='#{link_url}'>#{image_tag}</a>"
+      else
+        image_tag
+      end
+    end
 
-		def render(context)
-			if @img
-				"<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
-			else
-				"Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
-			end
-		end
-	end
+    def image_tag
+      "<img src='#{image_url}' title='#{title}' alt='#{alt}' />"
+    end
+
+    def load_options(contents)
+      contents.each do |line|
+        variable, value = parse_line(line)
+        instance_variable_set("@#{variable}", value)
+      end
+    end
+
+    def block_contents
+      text = @nodelist[0]
+      result = text.split(/\n/).map {|x| x.strip }.reject {|x| x.empty? }
+      result
+    end
+
+    # turn " blah: bloop bleep" into ["blah", "bloop bleep"]
+    def parse_line(line)
+      matches = line.match(/^\s*(\S+):(.*)/)
+      [matches.captures[0], matches.captures[1].strip]
+    end
+
+    def image_url
+      "/images/#{@image_name}"
+    end
+
+    def link?
+      !link_url.nil?
+    end
+
+    def link_url
+      @link
+    end
+
+    def title
+      @title || @image_name
+    end
+
+    def alt
+      @alt || title
+    end
+  end
 end
-                                                                             
-Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+Liquid::Template.register_tag('img', Jekyll::ImageBlock)
